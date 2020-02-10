798c1724336c0b9ec707971101885fdc4dbc8ba5
softdev9 <aprildev86@gmail.com>
2020-02-10 17:50:32 +0200

    added history and reducers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger';
 import './index.scss';
 import App from './App';
+import reducers from './redux/reducers';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import { createHashHistory } from 'history';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const history = createHashHistory
+const middlewares = [thunk]
+
+const store = createStore(reducers(history), compose(applyMiddleware(...middlewares)))
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

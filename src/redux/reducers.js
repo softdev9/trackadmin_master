a9fe3b655f727490199faa9b9b4c2829bed3ba78
softdev9 <aprildev86@gmail.com>
2020-02-14 09:45:54 +0200

    added setting reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,9 @@
 import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
+import settings from './settings/reducers'
 
-export default history => 
-  combineReducers({})
+export default history =>
+  combineReducers({
+    router: connectRouter(history),
+    settings,
+  })

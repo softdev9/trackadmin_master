408ebade77a1396bb837d68d89a667b0e48095f0
softdev9 <aprildev86@gmail.com>
2020-02-15 10:48:16 +0200

    integrated sagas

--- src/index.js
+++ src/index.js
@@ -7,18 +7,24 @@ import App from './App';
 import reducers from './redux/reducers';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose} from 'redux';
+import { routerMiddleware } from 'connected-react-router'
+import createSagaMiddleware from 'redux-saga'
 import thunk from 'redux-thunk';
 import { createHashHistory } from 'history';
 import Localization from './components/Layout/Localization';
 import Router from './router'
+import sagas from './redux/sagas'
 
 
 const history = createHashHistory()
-const middlewares = [thunk]
+const sagaMiddleware = createSagaMiddleware()
+const routeMiddleware = routerMiddleware(history)
+const middlewares = [thunk, sagaMiddleware, routeMiddleware]
 if (process.env.NODE_ENV === 'development' && true) {
   middlewares.push(logger)
 }
 const store = createStore(reducers(history), compose(applyMiddleware(...middlewares)))
+sagaMiddleware.run(sagas)
 
 ReactDOM.render(
     <Provider store={store}>

ca81e9f63a6e5d925ac1a5cd54d7d093226ec272
softdev9 <aprildev86@gmail.com>
2020-02-16 21:21:33 +0200

    dashboard page

--- src/pages/dashboard/alpha/index.js
+++ src/pages/dashboard/alpha/index.js
@@ -1,27 +1,28 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-// import Authorize from 'components/LayoutComponents/Authorize'
+import Authorize from 'components/Layout/Authorize'
 
 class DashboardAlpha extends React.Component {
-//   render() {
-//     return (
-//       <Authorize roles={['admin']} redirect to="/dashboard/beta">
-//         <Helmet title="Dashboard Alpha" />
-//         <div className="utils__title utils__title--flat mb-3">
-//           <strong className="text-uppercase font-size-16">Empty Page</strong>
-//         </div>
-//         <div className="row">
-//           <div className="col-lg-12">
-//             <div className="card">
-//               <div className="card-body">
-//                 [src\pages\dashboard\alpha\index.js]
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </Authorize>
-//     )
-//   }
+  render() {
+    console.log('Here is dashboard')
+    return (
+      <Authorize roles={['admin']} redirect to="/dashboard/beta">
+        <Helmet title="Dashboard Alpha" />
+        <div className="utils__title utils__title--flat mb-3">
+          <strong className="text-uppercase font-size-16">Empty Page</strong>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="card">
+              <div className="card-body">
+                [src\pages\dashboard\alpha\index.js]
+              </div>
+            </div>
+          </div>
+        </div>
+      </Authorize>
+    )
+  }
 }
 
 export default DashboardAlpha

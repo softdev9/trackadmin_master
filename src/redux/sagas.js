47068279d104db90d74a89b11d5b4a4b701dbba3
softdev9 <aprildev86@gmail.com>
2020-02-17 16:41:51 +0200

    menu sagas added

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects'
 import user from './user/sagas'
 import settings from './settings/sagas'
+import menu from './menu/sagas'
 
 export default function* rootSaga() {
-  yield all([user(), settings()])
+  yield all([user(), menu(), settings()])
 }

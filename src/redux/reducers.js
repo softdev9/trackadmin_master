8089189977890db8669e8d2b7f08923ed8caefd7
softdev9 <aprildev86@gmail.com>
2020-02-17 16:41:37 +0200

    menu redux added

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import settings from './settings/reducers'
 import user from './user/reducers'
+import menu from './menu/reducers'
 
 export default history =>
   combineReducers({
     router: connectRouter(history),
     user,
+    menu,
     settings,
   })

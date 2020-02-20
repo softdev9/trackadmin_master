702b8dd48a7a747ad6288f8ed5c0fc42129de218
softdev9 <aprildev86@gmail.com>
2020-02-20 17:57:38 +0200

    firebase login and logout

--- src/services/user.js
+++ src/services/user.js
@@ -1,32 +1,41 @@
-// import firebase from 'firebase/app'
+import firebase from 'firebase/app'
 import { notification } from 'antd'
-// import 'firebase/auth'
-// import 'firebase/database'
-// import 'firebase/storage'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/storage'
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyAE5G0RI2LwzwTBizhJbnRKIKbiXQIA1dY',
-//   authDomain: 'cleanui-72a42.firebaseapp.com',
-//   databaseURL: 'https://cleanui-72a42.firebaseio.com',
-//   projectId: 'cleanui-72a42',
-//   storageBucket: 'cleanui-72a42.appspot.com',
-//   messagingSenderId: '583382839121',
-// }
+const firebaseConfig = {
+  apiKey: 'AIzaSyAE5G0RI2LwzwTBizhJbnRKIKbiXQIA1dY',
+  authDomain: 'cleanui-72a42.firebaseapp.com',
+  databaseURL: 'https://cleanui-72a42.firebaseio.com',
+  projectId: 'cleanui-72a42',
+  storageBucket: 'cleanui-72a42.appspot.com',
+  messagingSenderId: '583382839121',
+}
 
-// const firebaseApp = firebase.initializeApp(firebaseConfig)
-// const firebaseAuth = firebase.auth
-// export default firebaseApp
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const firebaseAuth = firebase.auth
+export default firebaseApp
 
 export async function login(email, password) {
   console.log(`Login info: email: ${email} and password: ${password}`)
-    return await fetch(`http://127.0.0.1:8000/accounts/log-in/?email=${email}&password=${password}`, {mode: 'no-cors'})
+  return firebaseAuth()
+    .signInWithEmailAndPassword(email, password)
     .then(() => true)
-    .catch(error =>{
-        notification.warning({
-            message: error.code,
-            description: error.message,
-        })
+    .catch(error => {
+      notification.warning({
+        message: error.code,
+        description: error.message,
+      })
     })
+    // return await fetch(`http://127.0.0.1:8000/accounts/log-in/?email=${email}&password=${password}`, {mode: 'no-cors'})
+    // .then(() => true)
+    // .catch(error =>{
+    //     notification.warning({
+    //         message: error.code,
+    //         description: error.message,
+    //     })
+    // })
 }
 
 export async function currentAccount() {
@@ -47,12 +56,15 @@ export async function currentAccount() {
 }
 
 export async function logout() {
-    return await fetch(`http://127.0.0.1:8000/accounts/log-out`, {mode: 'no-cors'})
+    return firebaseAuth()
+    .signOut()
     .then(() => true)
-    .catch(error =>{
-        notification.warning({
-            message: error.code,
-            description: error.message,
-        })
-    })
+    // return await fetch(`http://127.0.0.1:8000/accounts/log-out`, {mode: 'no-cors'})
+    // .then(() => true)
+    // .catch(error =>{
+    //     notification.warning({
+    //         message: error.code,
+    //         description: error.message,
+    //     })
+    // })
 }
